af29d79acaa21bd1201e597831a0d9741a642082
OFFICE <chaitanyawadekar@Chaitanyas-MacBook-Pro-2.local>
2025-06-13 14:58:27 +0530

    Push new democratiq code to democratiq_ repo

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -52,11 +52,8 @@ export default function Page() {
         <div className="flex flex-1 flex-col">
           <div className="@container/main flex flex-1 flex-col gap-2">
             <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
-              {/*<SectionCards />
-              <div className="px-4 lg:px-6">
-                <ChartAreaInteractive />
-              </div>*/}
-              {console.log("before table" + JSON.stringify(tasks))}
+
+
               <DataTable data={tasks} />
             </div>
           </div>
